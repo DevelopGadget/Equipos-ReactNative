4fa04cd31d4e1583c2f3507bcb934c4459059ec8
FernanPro18 <fernandoaraujo081@gmail.com>
2018-05-12 20:35:38 -0500

    Responsividad por caja

--- src/Views/EquiposView.js
+++ src/Views/EquiposView.js
@@ -1,7 +1,7 @@
 'use strict'
 import React from 'react';
-import { StyleSheet, Text, View, Alert, Navigator, Image, Card } from 'react-native';
-import { Button } from 'react-native-elements';
+import { StyleSheet, Text, View, Alert, Navigator, Image, ScrollView, Dimensions } from 'react-native';
+import { Button, ListItem, Card } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { TabNavigator } from 'react-navigation';
 
@@ -11,29 +11,57 @@ export default class EquiposView extends React.Component {
     tabBarIcon: ({ focused,tintColor }) => (
       <Image
         source={require('../Views/Images/Team.png')}
-        style={[styles.icon, { color: tintColor }]}
+        style={[styles.Icon]}
       />
     ) 
   }
+  
   render() {
     return (
-      <View>
-        <Text>Hola Equipo</Text>
-        <Button title='Hola Mundo' onPress={(this.Alerta.bind(this))}/>
-        <Button large icon={{name: 'squirrel', type: 'octicon', buttonStyle: styles.someButtonStyle }} title='OCTICON' />
-      </View>
+      <ScrollView style={styles.ScrollContainer}>
+        <Button large icon={{name: 'plus-circle', type: 'font-awesome', buttonStyle: styles.Boton }} title='Añadir' />
+        <View style={styles.Container}>
+          <View style={styles.Box}/>
+          <View style={styles.Box}/>
+          <View style={styles.Box}/>
+          <View style={styles.Box}/>
+        </View>
+      </ScrollView>
     );
   }
-  Alerta(){
-  Alert.alert('Prueba','Prueba', [])
 }
-}
-
-module.exports = EquiposView;
 
 const styles = StyleSheet.create({
-  icon: {
+  Icon: {
     width: 30,
-    height: 30  
+    height: 30,
   },
+  ScrollContainer: {
+    flex : 1,
+  },
+  Container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 2,
+  },
+  Box: {
+    margin: 2,
+    width: Dimensions.get('window').width -6,
+    height: Dimensions.get('window').height / 1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1c40f',
+  },
+  Boton: {
+    backgroundColor: '#6200ea',
+    padding: 2,
+    margin: 2,
+    width: Dimensions.get('window').width,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
+
+module.exports = EquiposView;
